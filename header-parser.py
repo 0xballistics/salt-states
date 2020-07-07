585e751fb33bf0564265bdb1b46a707d0d4a8a35
~noyan <halilburaknoyan@gmail.com>
2020-07-07 23:34:41 +0300

    header-parser: we create sub-pages parsing semicolons in categories now.

--- header-parser.py
+++ header-parser.py
@@ -27,7 +27,7 @@ header_regex = re.compile(r"#\s*(?P<key>.*?):\s*(?P<value>.*)")
 def parse_header(file_path):
     fields = []
     name = None
-    categories = None
+    categories = []
     with open(file_path) as f:
         for line in f:
             line = line.strip()
@@ -42,7 +42,14 @@ def parse_header(file_path):
                     if key.lower() == "name":
                         name = value
                     elif key.lower() == "category":
-                        categories = [v.strip() for v in value.split(',') if v.strip()]
+                        if not value.strip():
+                            raise KeyError("categories field empty: %s" % file_path) 
+                        for v in value.split(','):
+                            full_cat = [vv.strip() for vv in v.split(':', 1)]
+                            if len(full_cat) == 1:
+                                full_cat.append("")
+                            categories.append(full_cat)
+
                         if len(categories) >= 2:
                             get_logger().info(f"{name} has more than 1 categories: {value}")
                     else:
@@ -64,7 +71,7 @@ def to_markdown(tool):
     header = "### {}\n".format(tool["name"])
     markdown = []
     tmp = "**{}**: {}"
-    markdown.append(tmp.format("Categories", ",".join(tool["categories"])))
+    # markdown.append(tmp.format("Categories", ",".join(tool["categories"])))
     for key, val in tool["fields"]:
         markdown.append(tmp.format(key,val))
 
@@ -109,7 +116,7 @@ def insert_page(cfg, title):
                 "title": title,
                 "description": "Discover The Tools",
                 "path": page_url,
-                "document": "" # setting content here does not work now.
+                "document": "" # setting content here does not work.
             }
         ]
     }
@@ -141,7 +148,12 @@ def get_gitbook_pages(cfg):
             raise RuntimeError(f"Cannot get gitbook pages! Response status: {resp.status_code}")
 
         js = resp.json()
-        d = {page["title"]: page["path"] for page in js["pages"]}
+        d = {}
+        for page in js["pages"]:
+            title = page["title"]
+            d[title] = {"": page["path"]}
+            for subpage in page["pages"]:
+                d[title][subpage["title"]] = subpage["path"]
         return d
     except Exception as e:
         get_logger().exception(e)
@@ -160,9 +172,10 @@ def main():
                     get_logger().error(str(e))
                     continue
 
-                for cat in d["categories"]:
-                    tools.setdefault(cat, [])
-                    tools[cat].append(d)
+                for cat, subcat in d["categories"]:
+                    tools.setdefault(cat, {})
+                    tools[cat].setdefault(subcat, [])
+                    tools[cat][subcat].append(d)
 
 
     with open("gitbook-config.json") as gitbook_config:
@@ -170,12 +183,25 @@ def main():
 
     pages = get_gitbook_pages(cfg)
 
-    for cat, tool_list  in sorted(tools.items()):
-        content = "\n\n".join([to_markdown(d) for d in tool_list])
-        if cat not in pages:
-            pages[cat] = insert_page(cfg, cat)
+    for cat, subcat_d  in sorted(tools.items()):
+        # check cat existence here
+        if cat in pages:
+            subpages = pages[cat]
+        else:
+            subpages = {"": insert_page(cfg, cat)}
 
-        update_page(cfg, pages[cat], content)
+        for subcat, tool_list in sorted(subcat_d.items()):
+            content = "\n\n".join([to_markdown(d) for d in tool_list])
+            
+            if subcat != "":
+                subcat_cfg = dict(cfg, parent_url=cfg["parent_url"]+"/"+subpages[''])
+            else:
+                subcat_cfg = cfg
+
+            if subcat not in subpages: 
+                subpages[subcat] = insert_page(subcat_cfg, subcat)
+
+            update_page(subcat_cfg, subpages[subcat], content)
 
 
 if __name__ == "__main__":
